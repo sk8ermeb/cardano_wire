622ee55b56eb7fcfe9e279e91a1ae33422867e04
Marvin <marvin.byrd@gmail.com>
2022-04-14 23:48:19 +0000

    a spot to come back to when I break things

--- settings.php
+++ settings.php
@@ -1,17 +1,16 @@
 <?php
 
-add_action('admin_menu', 'test_plugin_setup_menu');
+add_action('admin_menu', 'cardano_wire_setup_menu');
  
-function test_plugin_setup_menu(){
-    add_menu_page( 'Test Plugin Page', 'Test Plugin', 'manage_options', 'test-plugin', 'test_init' );
-		//add_options_page('Custom Plugin Page', 'Custom Plugin Menu', 'manage_options', 'plugin', 'plugin_options_page');
+function cardano_wire_setup_menu(){
+    add_menu_page( 'Cardano Wire Settings', 'Cardano Wire', 'manage_options', 'cardano_wire', 'cardano_wire_init' );
 }
  
-function test_init(){
+function cardano_wire_init(){
 ?>
 <div>
-<h2>My custom plugin</h2>
-Options relating to the Custom Plugin.
+<h2>Cardano Wire Settings</h2>
+Configuration for how to connect and extract data out of the blockchain and IPFS
 <form action="options.php" method="post">
 <?php settings_fields('plugin_options'); ?>
 <?php do_settings_sections('plugin'); ?>
@@ -22,8 +21,8 @@ Options relating to the Custom Plugin.
 <?php
     echo "<h1>Hello World!</h1>";
 }
-add_action('admin_init', 'plugin_admin_init');
-function plugin_admin_init(){
+add_action('admin_init', 'cardano_wire_admin_init');
+function cardano_wire_admin_init(){
 	register_setting( 'plugin_options', 'plugin_options', 'plugin_options_validate' );
 	add_settings_section('plugin_main', 'Main Settings', 'plugin_section_text', 'plugin');
 	add_settings_field('plugin_text_string', 'Plugin Text Input', 'plugin_setting_string', 'plugin', 'plugin_main');
@@ -48,19 +47,5 @@ function plugin_options_validate($input) {
 
 
 
-function plugin_options_page() {
-?>
-<div>
-<h2>My custom plugin</h2>
-Options relating to the Custom Plugin.
-<form action="options.php" method="post">
-<?php settings_fields('plugin_options'); ?>
-<?php do_settings_sections('plugin'); ?>
- 
-<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
-</form></div>
- 
-<?php
-}
 
 ?>
